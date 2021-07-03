4aae72043726ca65fbf5d97f93cddb6038f5e600
Adarsh Choutha <adarshchoutha@gmail.com>
2021-07-03 10:58:24 +0530

    stage dl_app

--- src/components/DLApp.js
+++ src/components/DLApp.js
@@ -7,15 +7,7 @@ function DLApp({ dl_data }) {
             <div className="dl-container">
                 <ul>
                     {dl_data.map((list, index) => {
-                        // const node = document.createElement("li");
-                        // node.draggable = true;
-                        // node.addEventListener('drag', setDragging);
-                        // node.addEventListener('dragover', setDraggedOver);
-                        // node.addEventListener('drop', compare);
-                        // node.innerText = list;
-                        return (
-                            <li key={"dl_li_" + index} id={"dl_li_" + index}>{list}</li>
-                        )
+                        return <li key={"dl_li_" + index} id={"dl_li_" + index}>{list}</li>
                     })}
                 </ul>
                 {document.addEventListener('DOMContentLoaded', () => {
@@ -24,29 +16,14 @@ function DLApp({ dl_data }) {
                         e.preventDefault();
                         const dragging_e = document.querySelector('.dragging');
                         const next_e = get_drag_nextElement(draggable_container, e.clientY);
-                        if (next_e == null) {
-                            draggable_container.appendChild(dragging_e);
-                        } else {
-                            draggable_container.insertBefore(dragging_e, next_e);
-                        }
+                        if (next_e == null) draggable_container.appendChild(dragging_e);
+                        else draggable_container.insertBefore(dragging_e, next_e);
                     };
                     dl_data.forEach((list, index) => {
                         const list_item = document.getElementById(`dl_li_${index}`);
                         list_item.draggable = true;
-
-                        list_item.ondragstart = e => {
-                            list_item.classList.add('dragging');
-                        };
-                        list_item.ondragend = e => {
-                            list_item.classList.remove('dragging');
-                        };
-                        // list_item.ondragstart = e => console.log("drag start:", e);
-                        // list_item.ondragenter = e => console.log("drag enter:", e);
-                        // list_item.ondragover = e => console.log("drag over:", e);
-                        // list_item.ondragexit = e => console.log("drag exit:", e);
-                        // list_item.ondrop = e => console.log("drag drop:", e);
-                        // list_item.ondragleave = e => console.log("drag leave:", e);
-                        // list_item.ondragend = e => console.log("drag end:", e);
+                        list_item.ondragstart = e => list_item.classList.add('dragging');
+                        list_item.ondragend = e => list_item.classList.remove('dragging');
                     })
                 })}
             </div>
